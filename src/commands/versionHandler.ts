bd10da5e8accbadd6a801149315a0b6dc72fbb81
Thim Lohse <thimdev@gmail.com>
2020-10-01 00:54:48 +0200

    reverted version import

--- src/commands/versionHandler.ts
+++ src/commands/versionHandler.ts
@@ -1,4 +1,5 @@
-import { version } from '../../package.json';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { version } = require('../../package.json');
 
 export default (): void => {
   // eslint-disable-next-line no-console
